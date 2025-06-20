38eb649cce0b456118441532e3cfb49079d99c25
kocerimustafayev <kocerimustafayev07@gmail.com>
2025-06-20 20:56:41 +0400

    Test

---
+++ src/main/java/com/classreport/classreport/ClassReportApplication.java
@@ -0,0 +1,13 @@
+package com.classreport.classreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassReportApplication.class, args);
+    }
+
+}

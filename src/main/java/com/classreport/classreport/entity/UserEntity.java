722bd8f9bd4bc2b9720d7a402daadbdd357e2f1b
kocerimustafayev <kocerimustafayev07@gmail.com>
2025-06-20 20:55:58 +0400

    Test

---
+++ src/main/java/com/classreport/classreport/entity/UserEntity.java
@@ -0,0 +1,15 @@
+package com.classreport.classreport.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserEntity {
+}
